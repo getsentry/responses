d4a5e3b06c783cfb94c9c4dbec62c16556b1bb03
Fabian Gentner <fabian.gentner@zalando.de>
2015-07-10 16:34:26 +0200

    Fix ConnectionError Emulation
    When raising a ConnectionError, the requests module sets the error's request attribute to the current request. The responses module should do the same.
    
    Sample Script:
        import requests
        import responses
    
        def req():
            try:
                requests.get('http://example.zalando.net')
            except Exception as e:
                return(e.request)
            else:
                assert False
    
        print('Real:          ', req())
        print('With responses:', responses.activate(req)())
    
    Output:
        Real:           <PreparedRequest [GET]>
        With responses: None

--- responses.py
+++ responses.py
@@ -225,7 +225,7 @@ class RequestsMock(object):
         if match is None:
             error_msg = 'Connection refused: {0} {1}'.format(request.method,
                                                              request.url)
-            response = ConnectionError(error_msg)
+            response = ConnectionError(error_msg, request=request)
 
             self._calls.add(request, response)
             raise response

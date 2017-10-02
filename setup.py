3e109b3cd5f4870d117e27924fb6ce9f16015584
David Cramer <dcramer@gmail.com>
2017-10-02 10:01:23 -0700

    0.8.1

--- setup.py
+++ setup.py
@@ -69,7 +69,7 @@ class PyTest(TestCommand):
 
 setup(
     name='responses',
-    version='0.8.0',
+    version='0.8.1',
     author='David Cramer',
     description=(
         'A utility library for mocking out the `requests` Python library.'),

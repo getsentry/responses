8525b0213929709956f5be60c3729887b1dbe079
Farhan Kathawala <farhank@stanford.edu>
2017-10-21 21:49:44 -0700

    Backwards compatibility with requests fix

--- responses.py
+++ responses.py
@@ -561,7 +561,7 @@ class RequestsMock(object):
 
         def unbound_on_send(adapter, request, *a, **kwargs):
             retries = adapter.max_retries if isinstance(adapter.max_retries, int) else adapter.max_retries.total
-            return self._on_request(adapter, request, adapter.max_retries.total, *a, **kwargs)
+            return self._on_request(adapter, request, retries, *a, **kwargs)
 
         self._patcher = mock.patch('requests.adapters.HTTPAdapter.send',
                                    unbound_on_send)

a0a72771ad5a9aa09aa0a5aa3845a6d53d80f0a4
Mark Story <mark@sentry.io>
2019-11-25 11:13:08 -0500

    Bump version to dev.

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ class PyTest(TestCommand):
 
 setup(
     name="responses",
-    version="0.10.7",
+    version="0.11.0-dev",
     author="David Cramer",
     description=("A utility library for mocking out the `requests` Python library."),
     url="https://github.com/getsentry/responses",

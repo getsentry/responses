34bb69a5e18193b289d1c49e244e152d48aa47bd
Tessa Alexanian <tessa@zymergen.com>
2019-12-12 16:52:13 -0800

    Update python_requires to match content of the python_requires in requests

--- setup.py
+++ setup.py
@@ -63,7 +63,7 @@ setup(
     long_description_content_type="text/x-rst",
     py_modules=["responses"],
     zip_safe=False,
-    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*",
     install_requires=install_requires,
     extras_require=extras_require,
     tests_require=tests_require,

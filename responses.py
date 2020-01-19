7d282304a1bc50f1b11a4db6f9fce81bd4ad87a2
Hasan Ramezani <hasan.r67@gmail.com>
2020-01-19 20:36:05 +0100

    Return requested params in responses.calls[].request. Fix #271

--- responses.py
+++ responses.py
@@ -622,6 +622,7 @@ class RequestsMock(object):
     def _on_request(self, adapter, request, **kwargs):
         match = self._find_match(request)
         resp_callback = self.response_callback
+        request.params = dict(parse_qsl(request.path_url.replace("/?", "")))
 
         if match is None:
             if request.url.startswith(self.passthru_prefixes):

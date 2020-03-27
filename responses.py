getsentry/responses

commit eddcb95238d49555ecfb66967b8cab9a34809755

Change behavior for multiple matches per PR comment

responses.py
@@ -386,15 +386,17 @@ class RequestsMock(object):
         return get_wrapped(func, _wrapper_template, evaldict)
 
     def _find_match(self, request):
+        found = None
+        found_match = None
         for i, match in enumerate(self._matches):
             if match.matches(request):
-                break
-        else:
-            return None
-        # move match to end
-        self._matches.pop(i)
-        self._matches.append(match)
-        return match
+                if found is None:
+                    found = i
+                    found_match = match
+                else:
+                    # Multiple matches found.  Remove & return the first match.
+                    return self._matches.pop(found)
+        return found_match
 
     def _on_request(self, adapter, request, **kwargs):
         match = self._find_match(request)

test_responses.py
@@ -606,6 +606,9 @@ def test_multiple_responses():
         assert_response(resp, 'test')
         resp = requests.get('http://example.com')
         assert_response(resp, 'rest')
+        # After all responses are used, last response should be repeated
+        resp = requests.get('http://example.com')
+        assert_response(resp, 'rest')
 
     run()
     assert_reset()

commit 58e2c66c9e4994cff0702300608fefa8a6376eb0

Stop deleting a CallbackResponse if more than one of them matches a HTTP request
Regression introduced in commit eddcb95238d49555ecfb66967b8cab9a34809755.

responses.py
@@ -617,18 +617,9 @@ class RequestsMock(object):
         return get_wrapped(func, self)
 
     def _find_match(self, request):
-        found = None
-        found_match = None
         for i, match in enumerate(self._matches):
             if match.matches(request):
-                if found is None:
-                    found = i
-                    found_match = match
-                else:
-                    # Multiple matches found.  Remove & return the first match.
-                    return self._matches.pop(found)
-
-        return found_match
+                return match
 
     def _on_request(self, adapter, request, **kwargs):
         match = self._find_match(request)
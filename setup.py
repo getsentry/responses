1f2d9e9efcd66eaadb94130ba31220f00b263e8d
Mark Story <mark@mark-story.com>
2020-06-11 22:18:06 -0400

    Update changelog and version.

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ class PyTest(TestCommand):
 
 setup(
     name="responses",
-    version="0.10.15-dev",
+    version="0.10.15",
     author="David Cramer",
     description=("A utility library for mocking out the `requests` Python library."),
     url="https://github.com/getsentry/responses",

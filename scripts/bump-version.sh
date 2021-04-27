2dbaba5480d33ae9af340d4bb31dc704880431c7
Chad Whitacre <chadwhitacre@sentry.io>
2021-04-27 14:38:20 -0400

    Wire into publish repo (#383)

---
+++ scripts/bump-version.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -eux
+
+cd "$(dirname "$0")/.."
+
+OLD_VERSION="${1}"
+NEW_VERSION="${2}"
+
+echo "Current version: $OLD_VERSION"
+echo "Bumping version: $NEW_VERSION"
+
+function replace() {
+    ! grep "$2" "$3"
+    sed -e "s/$1/$2/g" "$3" > "$3.tmp"  # -i is non-portable
+    mv "$3.tmp" "$3"
+    grep "$2" "$3"  # verify that replacement was successful
+}
+
+replace "version='[^']*'" "version='$NEW_VERSION'" ./setup.py

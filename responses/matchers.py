374e605b2efc2207b719c53a01c7e5a20e39b0cf
Maksim Beliaev <beliaev.m.s@gmail.com>
2021-10-18 15:45:03 +0200

    mbeliaev/kwargs check (#403)
    Add new request matcher for additional keyword args that are passed to requests.

--- responses/matchers.py
+++ responses/matchers.py
@@ -12,24 +12,65 @@ except ImportError:
     JSONDecodeError = ValueError
 
 
+def _create_key_val_str(input_dict):
+    """
+    Returns string of format {'key': val, 'key2': val2}
+    Function is called recursively for nested dictionaries
+
+    :param input_dict: dictionary to transform
+    :return: (str) reformatted string
+    """
+
+    def list_to_str(input_list):
+        """
+        Convert all list items to string.
+        Function is called recursively for nested lists
+        """
+        converted_list = []
+        for item in sorted(input_list, key=lambda x: str(x)):
+            if isinstance(item, dict):
+                item = _create_key_val_str(item)
+            elif isinstance(item, list):
+                item = list_to_str(item)
+
+            converted_list.append(str(item))
+        list_str = ", ".join(converted_list)
+        return "[" + list_str + "]"
+
+    items_list = []
+    for key in sorted(input_dict.keys(), key=lambda x: str(x)):
+        val = input_dict[key]
+        if isinstance(val, dict):
+            val = _create_key_val_str(val)
+        elif isinstance(val, list):
+            val = list_to_str(input_list=val)
+
+        items_list.append("{}: {}".format(key, val))
+
+    key_val_str = "{{{}}}".format(", ".join(items_list))
+    return key_val_str
+
+
 def urlencoded_params_matcher(params):
     """
     Matches URL encoded data
+
     :param params: (dict) data provided to 'data' arg of request
     :return: (func) matcher
     """
 
     def match(request):
+        reason = ""
         request_body = request.body
-        valid = (
-            params is None
-            if request_body is None
-            else sorted(params.items()) == sorted(parse_qsl(request_body))
-        )
+        qsl_body = dict(parse_qsl(request_body)) if request_body else {}
+        params_dict = params or {}
+        valid = params is None if request_body is None else params_dict == qsl_body
         if not valid:
-            return False, "%s doesn't match %s" % (request_body, params)
+            reason = "request.body doesn't match: {} doesn't match {}".format(
+                _create_key_val_str(qsl_body), _create_key_val_str(params_dict)
+            )
 
-        return valid, ""
+        return valid, reason
 
     return match
 
@@ -37,26 +78,34 @@ def urlencoded_params_matcher(params):
 def json_params_matcher(params):
     """
     Matches JSON encoded data
+
     :param params: (dict) JSON data provided to 'json' arg of request
     :return: (func) matcher
     """
 
     def match(request):
+        reason = ""
         request_body = request.body
+        params_dict = params or {}
         try:
             if isinstance(request_body, bytes):
                 request_body = request_body.decode("utf-8")
-            valid = (
-                params is None
-                if request_body is None
-                else params == json_module.loads(request_body)
-            )
-            if not valid:
-                return False, "%s doesn't match %s" % (request_body, params)
+            json_body = json_module.loads(request_body) if request_body else {}
 
-            return valid, ""
+            valid = params is None if request_body is None else params_dict == json_body
+
+            if not valid:
+                reason = "request.body doesn't match: {} doesn't match {}".format(
+                    _create_key_val_str(json_body), _create_key_val_str(params_dict)
+                )
+
         except JSONDecodeError:
-            return False, "JSONDecodeError: Cannot parse request.body"
+            valid = False
+            reason = (
+                "request.body doesn't match: JSONDecodeError: Cannot parse request.body"
+            )
+
+        return valid, reason
 
     return match
 
@@ -64,24 +113,60 @@ def json_params_matcher(params):
 def query_param_matcher(params):
     """
     Matcher to match 'params' argument in request
+
     :param params: (dict), same as provided to request
     :return: (func) matcher
     """
 
     def match(request):
+        reason = ""
         request_params = request.params
+        request_params_dict = request_params or {}
+        params_dict = params or {}
         valid = (
             params is None
             if request_params is None
-            else sorted(params.items()) == sorted(request_params.items())
+            else params_dict == request_params_dict
         )
 
         if not valid:
-            return False, "%s doesn't match %s" % (
-                sorted(request_params.items()),
-                sorted(params.items()),
+            reason = "Parameters do not match. {} doesn't match {}".format(
+                _create_key_val_str(request_params_dict),
+                _create_key_val_str(params_dict),
             )
 
-        return valid, ""
+        return valid, reason
+
+    return match
+
+
+def request_kwargs_matcher(kwargs):
+    """
+    Matcher to match keyword arguments provided to request
+
+    :param kwargs: (dict), keyword arguments, same as provided to request
+    :return: (func) matcher
+    """
+
+    def match(request):
+        reason = ""
+        kwargs_dict = kwargs or {}
+        # validate only kwargs that were requested for comparison, skip defaults
+        request_kwargs = {
+            k: v for k, v in request.req_kwargs.items() if k in kwargs_dict
+        }
+
+        valid = (
+            not kwargs_dict
+            if not request_kwargs
+            else sorted(kwargs.items()) == sorted(request_kwargs.items())
+        )
+
+        if not valid:
+            reason = "Arguments don't match: {} doesn't match {}".format(
+                _create_key_val_str(request_kwargs), _create_key_val_str(kwargs_dict)
+            )
+
+        return valid, reason
 
     return match

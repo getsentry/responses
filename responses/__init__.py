a60b7e17ab5ed9f0cbd524fd3dad0a4a6a01c78b
Eli Carter <eli.carter@synchronoss.com>
2021-10-27 15:54:43 -0500

    Special handling of the stream flags appears counter-productive
    Fixes #421

--- responses/__init__.py
+++ responses/__init__.py
@@ -780,14 +780,6 @@ class RequestsMock(object):
                 response = resp_callback(response) if resp_callback else response
                 raise
 
-        stream = kwargs.get("stream") if match.stream is None else match.stream
-        if not stream:
-            content = response.content
-            if kwargs.get("stream"):
-                response.raw = BufferIO(content)
-            else:
-                response.close()
-
         response = resp_callback(response) if resp_callback else response
         match.call_count += 1
         self._calls.add(request, response)

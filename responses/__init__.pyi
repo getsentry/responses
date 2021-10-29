54a7f9d1dc1f4729162b0135718bb7716187ce4f
Mark Story <mark@sentry.io>
2021-10-29 10:16:05 -0400

    Fix regression from stream deprecation (#422)
    Not closing the response results in cookies not being persisted to the
    requests session.
    
    Thanks to @beliaev-maksim for help with the tests.
    
    Fixes #421

--- responses/__init__.pyi
+++ responses/__init__.pyi
@@ -182,6 +182,8 @@ class RequestsMock:
 
 _F = TypeVar("_F", bound=Callable[..., Any])
 
+HeaderSet = Optional[Union[Mapping[str, str], List[Tuple[str, str]]]]
+
 class _Activate(Protocol):
     def __call__(self, func: _F) -> _F: ...
 
@@ -193,11 +195,11 @@ class _Add(Protocol):
         body: _Body = ...,
         json: Optional[Any] = ...,
         status: int = ...,
-        headers: Optional[Mapping[str, str]] = ...,
+        headers: HeaderSet = ...,
         stream: bool = ...,
         content_type: Optional[str] = ...,
         auto_calculate_content_length: bool = ...,
-        adding_headers: Optional[Mapping[str, str]] = ...,
+        adding_headers: HeaderSet = ...,
         match_querystring: bool = ...,
         match: List[Any] = ...,
     ) -> None: ...
@@ -232,10 +234,10 @@ class _Replace(Protocol):
         body: _Body = ...,
         json: Optional[Any] = ...,
         status: int = ...,
-        headers: Optional[Mapping[str, str]] = ...,
+        headers: HeaderSet = ...,
         stream: bool = ...,
         content_type: Optional[str] = ...,
-        adding_headers: Optional[Mapping[str, str]] = ...,
+        adding_headers: HeaderSet = ...,
         match_querystring: bool = ...,
         match: List[Any] = ...,
     ) -> None: ...
@@ -248,10 +250,10 @@ class _Upsert(Protocol):
         body: _Body = ...,
         json: Optional[Any] = ...,
         status: int = ...,
-        headers: Optional[Mapping[str, str]] = ...,
+        headers: HeaderSet = ...,
         stream: bool = ...,
         content_type: Optional[str] = ...,
-        adding_headers: Optional[Mapping[str, str]] = ...,
+        adding_headers: HeaderSet = ...,
         match_querystring: bool = ...,
         match: List[Any] = ...,
     ) -> None: ...

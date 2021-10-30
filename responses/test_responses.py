e7790ecc0a591155eb18ab5231340d5a8d3699bb
Maksim Beliaev <beliaev.m.s@gmail.com>
2021-10-30 02:08:40 +0200

    clean up test

--- responses/test_responses.py
+++ responses/test_responses.py
@@ -810,14 +810,10 @@ def test_activate_doesnt_change_signature_with_return_type():
     test_function.__annotations__["a"] = Mock
 
     decorated_test_function = responses.activate(test_function)
-    if hasattr(inspect, "signature"):
-        assert inspect.signature(test_function) == inspect.signature(
-            decorated_test_function
-        )
-    else:
-        assert inspect.getargspec(test_function) == inspect.getargspec(
-            decorated_test_function
-        )
+    assert inspect.signature(test_function) == inspect.signature(
+        decorated_test_function
+    )
+
     assert decorated_test_function(1, 2) == test_function(1, 2)
     assert decorated_test_function(3) == test_function(3)
 

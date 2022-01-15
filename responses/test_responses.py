d078136d0ccc1af1364e7e003ebeb7d0538ac924
Maksim Beliaev <beliaev.m.s@gmail.com>
2022-01-15 01:55:40 +0100

    added async test

--- responses/test_responses.py
+++ responses/test_responses.py
@@ -1927,17 +1927,19 @@ def test_requests_between_add():
     assert_reset()
 
 
-@responses.activate
 async def test_async_calls():
-    responses.add(
-        responses.GET,
-        "http://twitter.com/api/1/foobar",
-        json={"error": "not found"},
-        status=404,
-    )
+    @responses.activate
+    async def run():
+        responses.add(
+            responses.GET,
+            "http://twitter.com/api/1/foobar",
+            json={"error": "not found"},
+            status=404,
+        )
 
-    resp = requests.get("http://twitter.com/api/1/foobar")
+        resp = requests.get("http://twitter.com/api/1/foobar")
+        assert resp.json() == {"error": "not found"}
+        assert responses.calls[0].request.url == "http://twitter.com/api/1/foobar"
 
-    assert resp.json() == {"error": "not found"}
-    assert responses.calls[0].request.url == "http://twitter.com/api/1/foobar"
+    await run()
     assert_reset()

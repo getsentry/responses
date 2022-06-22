97c7b27adf8be3c794a4ef43e0a30dbe2c1f400d
Maksim Beliaev <beliaev.m.s@gmail.com>
2022-06-22 17:30:55 +0200

    fix #556 mypy patcher complaints on namespace packages

--- responses/__init__.py
+++ responses/__init__.py
@@ -63,7 +63,8 @@ from urllib.parse import urlunsplit
 
 if TYPE_CHECKING:  # pragma: no cover
     # import only for linter run
-    from mypy.typeshed.stdlib.unittest.mock import _patcher as _mock_patcher
+    from unittest.mock import _patch as _mock_patcher
+
     from requests import PreparedRequest
     from requests import models
     from urllib3 import Retry as _Retry
@@ -652,7 +653,7 @@ class RequestsMock(object):
         self.response_callback: Optional[Callable[[Any], Response]] = response_callback
         self.passthru_prefixes: Tuple[_URLPatternType, ...] = tuple(passthru_prefixes)
         self.target: str = target
-        self._patcher: Optional["_mock_patcher"] = None
+        self._patcher: Optional["_mock_patcher[Any]"] = None
         self._thread_lock = _ThreadingLock()
 
     def get_registry(self) -> FirstMatchRegistry:

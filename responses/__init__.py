9dfd3d8eed02db6957db718511c08439da2215e3
Maksim Beliaev <beliaev.m.s@gmail.com>
2023-10-04 09:56:47 +0200

    fix mypy

--- responses/__init__.py
+++ responses/__init__.py
@@ -49,7 +49,8 @@ except ImportError:  # pragma: no cover
 try:
     from requests.packages.urllib3.connection import HTTPHeaderDict
 except ImportError:  # pragma: no cover
-    from urllib3.response import HTTPHeaderDict  # type: ignore[attr-defined]
+    from urllib3.response import HTTPHeaderDict
+
 try:
     from requests.packages.urllib3.util.url import parse_url
 except ImportError:  # pragma: no cover
